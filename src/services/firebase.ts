186aa21ff4018dcba426368e4188acda834b2004
2005balaji <balajisr000@gmail.com>
2023-04-26 19:36:39 +0530

    configured firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,8 +1,37 @@
-import { initializeApp } from 'firebase-admin/app'
-import { getFirestore } from 'firebase-admin/firestore'
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { credential } from 'firebase-admin';
 
-// Initialize Firebase Admin
-export const app = initializeApp()
+// Firebase service account credentials
+const serviceAccount = require('./firebase-adminsdk.json');
 
-// Initialize Firestore
-export const db = getFirestore(app)
+
+//fire base config 
+const firebaseConfig = {
+    apiKey: "AIzaSyCsJ6UHp7ttyGasmEVixaJTRGygGLjYUyU",
+    authDomain: "technical-task-1566a.firebaseapp.com",
+    projectId: "technical-task-1566a",
+    storageBucket: "technical-task-1566a.appspot.com",
+    messagingSenderId: "735077177798",
+    appId: "1:735077177798:web:2d4837a8e5dc2a7d7cbac7",
+    measurementId: "G-T4622DDYWB"
+}
+
+
+// // Initialize Firebase Admin
+// const app = initializeApp(firebaseConfig);    
+
+
+// // Initialize Firestore
+// export const db = getFirestore(app)
+
+
+
+// Initialize Firebase Admin with the service account credentials
+const app = initializeApp({
+    credential: credential.cert(serviceAccount),
+    ...firebaseConfig,
+  });
+  
+  // Initialize Firestore
+  export const db = getFirestore(app);
